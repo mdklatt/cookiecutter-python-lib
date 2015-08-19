9f6fc5b54d11f5fd15c7a3bac691aa742efeea77
Michael Klatt <mdklatt@ou.edu>
2015-08-19 00:30:20 -0500

    added template test suite

---
+++ {{cookiecutter.repo_name}}/test/test_lib.py
@@ -0,0 +1,25 @@
+""" Test suite for the {{cookiecutter.repo_name}} library template.
+
+The script can be executed on its own or incorporated into a larger test suite.
+However the tests are run, be aware of which version of the package is actually
+being tested. If the package is installed in site-packages, that version takes
+precedence over the version in this project directory. Use a virtualenv test
+environment or setuptools develop mode to test against the development version.
+
+"""
+import pytest
+
+
+def test_version():
+    """ Test the library version.
+
+    """
+    from {{cookiecutter.repo_name}} import __version__
+    assert __version__ == "{{cookiecutter.project_version}}"
+    return
+
+
+# Make the module executable.
+
+if __name__ == "__main__":
+    raise SystemExit(pytest.main(__file__))
