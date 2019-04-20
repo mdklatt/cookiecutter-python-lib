33aa920380d2d06c47f7fa73f35b5138353e4671
Michael Klatt <mdklatt@alumni.ou.edu>
2019-04-20 18:14:57 -0500

    Refactored setup.py in project

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,107 +1,37 @@
 """ Setup script for the {{ cookiecutter.lib_name }} library.
 
 """
-from setuptools import Command
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
-from distutils import log
-from os.path import join
-from subprocess import check_call
-from subprocess import CalledProcessError
 
 
-_CONFIG = {
+_config = {
     "name": "{{ cookiecutter.lib_name }}",
     "author": "{{ cookiecutter.author_name   }}",
     "author_email": "{{ cookiecutter.author_email }}",
     "url": "",
     "package_dir": {"": "src"},
-    "packages": find_packages("src")}
-
-
-def _version():
-    """ Get the local package version.
-
-    """
-    path = join("src", _CONFIG["name"], "__version__.py")
-    namespace = {}
-    with open(path) as stream:
-        exec(stream.read(), namespace)
-    return namespace["__version__"]
-
-
-class _CustomCommand(Command):
-    """ Abstract base class for a custom setup command.
-
-    """
-    # Each user option is a tuple consisting of the option's long name (ending
-    # with "=" if it accepts an argument), its single-character alias, and a
-    # description.
-    description = ""
-    user_options = []  # this must be a list
-
-    def initialize_options(self):
-        """ Set the default values for all user options.
-
-        """
-        return
-
-    def finalize_options(self):
-        """ Set final values for all user options.
-
-        This is run after all other option assignments have been completed
-        (e.g. command-line options, other commands, etc.)
-
-        """
-        return
-
-    def run(self):
-        """ Execute the command.
-
-        Raise SystemExit to indicate failure.
-
-        """
-        raise NotImplementedError
-
-
-class UpdateCommand(_CustomCommand):
-    """ Custom setup command to pull from a remote branch.
-
-    """
-    description = "update from a remote branch"
-    user_options = [
-        ("remote=", "r", "remote name [default: tracking remote]"),
-        ("branch=", "b", "branch name [default: tracking branch]")]
-
-    def initialize_options(self):
-        """ Set the default values for all user options.
-
-        """
-        self.remote = ""  # default to tracking remote
-        self.branch = ""  # default to tracking branch
-        return
-
-    def run(self):
-        """ Execute the command.
-
-        """
-        args = {"remote": self.remote, "branch": self.branch}
-        cmdl = "git pull --ff-only {remote:s} {branch:s}".format(**args)
-        try:
-            check_call(cmdl.split())
-        except CalledProcessError:
-            raise SystemExit(1)
-        log.info("package version is now {:s}".format(version()))
-        return
+    "packages": find_packages("src")
+}
 
 
 def main():
     """ Execute the setup commands.
 
     """
-    _CONFIG["version"] = _version()
-    _CONFIG["cmdclass"] = {"update": UpdateCommand}
-    setup(**_CONFIG)
+    def version():
+        """ Get the local package version. """
+        namespace = {}
+        path = Path("src", _config["name"], "__version__.py")
+        exec(path.read_text(), namespace)
+        return namespace["__version__"]
+
+    _config.update({
+        "version": version(),
+    })
+    setup(**_config)
     return 0
 
 
